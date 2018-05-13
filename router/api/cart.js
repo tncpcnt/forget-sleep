ebde40871b5387bd797f4be367e72ba843357ac6
Pannawat Nokket <nogket@gmail.com>
2018-05-14 03:38:16 +0700

    invoice

--- router/api/cart.js
+++ router/api/cart.js
@@ -4,6 +4,8 @@ module.exports = (() => {
     const router = express.Router();
     var Cart = require('../../models/cart');
     var Product = require('../../models/product');
+    var user = require('../../models/user');
+
     // middleware to use for all requests
     // const checkAuth = (req, res, next) => {
     //     if (!req.session.user_id) {
@@ -45,8 +47,58 @@ module.exports = (() => {
                 });
             }
         });
-       
+    });
 
+    router.get('/invoice', function (req, res) {
+        var username = req.session.username
+        console.log(username);
+        var query = {
+            username: username
+        };
+        var order = Math.floor((Math.random()*100000) + 1)
+
+        user.findOne(query, function (err, myuser) {
+            console.log(err);
+            console.log(myuser);
+            req.session.invoice = req.session.cart
+            delete req.session.cart
+            var cart = new Cart(req.session.invoice);
+            res.json({
+                items: cart.generateArray(),
+                totalPrice: cart.totalPrice,
+                'qty': req.session.qty,
+                date: new Date().toISOString().slice(0,10),
+                order: order,
+                name: myuser.firstname,
+                lastname: myuser.lastname,
+                address: myuser.address,
+                tell: myuser.tell,
+                email: myuser.email
+            });
+        });
+    });
+
+    router.get('/success', function (req, res) {
+        req.session.invoice = req.session.cart
+        var cart = new Cart(req.session.cart);
+        req.session.qty = cart.totalQty
+        var temp = []
+        items = cart.generateArray()
+        items.forEach(element => {
+            temp.push(element.item._id)
+            Product.findOne({
+                '_id': element.item._id
+            }, function (err, product) {
+                if (product) {
+                    product.quantity--
+                        product.save(function (err) {
+                            if (err)
+                                console.log(err);
+                        })
+                }
+            });
+        });
+        res.redirect("/invoice.html");
     });
 
     router.get('/cart', function (req, res) {
@@ -66,7 +118,7 @@ module.exports = (() => {
         }
     });
 
-    
+
 
     router.get('/check_cart', function (req, res) {
         res.json({
